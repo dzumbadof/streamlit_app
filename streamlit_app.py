5bedb59b8287030cf16a309b416c6086e2e2c8e0
dzumbadof <49890266+dzumbadof@users.noreply.github.com>
2023-07-03 21:51:36 -0600

    Update streamlit_app.py
    Added default values

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,5 +17,5 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.dataframe(my_fruit_list)
 
 #Added multiselect 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 
